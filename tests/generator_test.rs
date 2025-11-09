db9d6412087e0645c0b24037ef237b9ca0093a4f
Ovikx <ovikdas10@gmail.com>
2025-11-09 01:30:00 -0600

    add sample sequence and caching tests

---
+++ tests/generator_test.rs
@@ -0,0 +1,79 @@
+use std::{cell::RefCell, rc::Rc};
+
+use audio_gen::{
+    context::audio_context::AudioContext,
+    generator::SampleGenerator,
+    node::{float::Float32Source, sine_oscillator::SineOscillatorNode, sum::SumNode},
+    source::CachedFloatSource,
+};
+
+fn threshold_eq_float32(value: f32, target: f32) -> bool {
+    let margin = 0.0001;
+    target - margin <= value && target + margin >= value
+}
+
+#[test]
+fn test_sine_oscillator_node_sequence() {
+    let oscillator_node = SineOscillatorNode::new(Rc::new(RefCell::new(CachedFloatSource::new(
+        Box::new(Float32Source::new(1.)),
+    ))));
+
+    let sample_rate = 4.;
+    let mut generator = SampleGenerator::new(
+        Rc::new(RefCell::new(CachedFloatSource::new(Box::new(
+            oscillator_node,
+        )))),
+        AudioContext::new(sample_rate),
+    );
+
+    let num_sets = 100;
+    let samples = generator.generate_samples(4 * num_sets + 1);
+    let expected_samples: Vec<f32> = vec![1.0, 0.0, -1.0, 0.0];
+    assert!(threshold_eq_float32(samples[0], 0.));
+    dbg!(&samples);
+    for i in 1..num_sets * 4 {
+        dbg!(
+            samples[i as usize],
+            expected_samples[((i as u32) % 4) as usize]
+        );
+        assert!(threshold_eq_float32(
+            samples[i as usize],
+            expected_samples[(((i - 1) as u32) % 4) as usize]
+        ));
+    }
+}
+
+#[test]
+fn test_cached_sequence() {
+    let cached_oscillator_node = Rc::new(RefCell::new(CachedFloatSource::new(Box::new(
+        SineOscillatorNode::new(Rc::new(RefCell::new(CachedFloatSource::new(Box::new(
+            Float32Source::new(1.),
+        ))))),
+    ))));
+
+    let sum_node = Rc::new(RefCell::new(CachedFloatSource::new(Box::new(
+        SumNode::new(
+            Rc::clone(&cached_oscillator_node),
+            Rc::clone(&cached_oscillator_node),
+        ),
+    ))));
+
+    let sample_rate = 4.;
+    let mut generator = SampleGenerator::new(Rc::clone(&sum_node), AudioContext::new(sample_rate));
+
+    let num_sets = 100;
+    let samples = generator.generate_samples(4 * num_sets + 1);
+    let expected_samples: Vec<f32> = vec![2.0, 0.0, -2.0, 0.0];
+    assert!(threshold_eq_float32(samples[0], 0.));
+    dbg!(&samples);
+    for i in 1..num_sets * 4 {
+        dbg!(
+            samples[i as usize],
+            expected_samples[((i as u32) % 4) as usize]
+        );
+        assert!(threshold_eq_float32(
+            samples[i as usize],
+            expected_samples[(((i - 1) as u32) % 4) as usize]
+        ));
+    }
+}
